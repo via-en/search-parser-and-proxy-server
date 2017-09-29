fae167f944cae0796724199f22ab8dd4f19d2bd6
Victoria Khasanova <virna2010@gmail.com>
2017-09-29 12:18:42 +0300

    add CreatedDate

--- docker/project/proccess/main.py
+++ docker/project/proccess/main.py
@@ -22,7 +22,8 @@ class SomeTaskManager(TaskManager):
   def _callback(self, task):
     self._logger.debug(task.crawlID)
 
-    params = {'snTag': task.snTag, 'CrawlId': task.crawlID, 'workflowID': task.workflowID}
+    params = {'snTag': task.snTag, 'CrawlId': task.crawlID, 'workflowID': task.workflowID,
+              'CreatedDate': task.CreatedDate}
     payload = {'text': task.search_q}
 
     patch_record_factory(sn_tag=task.snTag, version='0.1.1', crawl_id=task.crawlID)
